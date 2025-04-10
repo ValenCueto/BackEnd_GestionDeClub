da4523e3dcb1a13b13a468bca6c0fc26634a45e5
Juan Martin Masqueda <j.masqueda@gmail.com>
2025-04-10 14:30:07 -0300

    Creacion de entidades

---
+++ src/Domain/Entities/Availability.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Availability
+    {
+    
+        public int Id { get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly FinishTime { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
---
+++ src/Domain/Entities/CourtAvailability.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CourtAvailability
+    {
+  
+
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime FinishTime { get; set; }
+        public bool Available { get; set; }
+    }
+}
