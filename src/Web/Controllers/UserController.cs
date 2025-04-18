18929ef0245215658cbc4903cf9574679bc0c162
Agustin <agustinferraresi5@gmail.com>
2025-04-18 17:46:47 -0300

    Agrega try y catch al endpoint de create user

--- src/Web/Controllers/UserController.cs
+++ src/Web/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Interfaces;
 using Application.Models.Request;
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
@@ -17,19 +18,26 @@ namespace Web.Controllers
         [HttpPost]
         public IActionResult CreateUser([FromBody] UserCreateRequest request)
         {
-             _userService.CreateUser(request);
-            return Ok("ok");
-        }
-
-        [HttpPost("UpdateUser")]
-        public IActionResult UpdateUser([FromBody] UserCreateRequest request, int userId)
-        {
             try
             {
-                _userService.UpdateUser(request, userId);
-                return Ok("Usuario editado exitosamente");
+                _userService.CreateUser(request);
+                return Ok("Usuario creado exitosamente");
             }
             catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+         [HttpPost("UpdateUser")]
+        public IActionResult UpdateUser([FromBody] UserCreateRequest request, int userId)
+        {
+            try 
+            {
+                _userService.UpdateUser(request,userId);
+                return Ok("Usuario editado exitosamente");
+            }
+            catch (Exception ex) 
             {
                 return BadRequest(ex);
             }
