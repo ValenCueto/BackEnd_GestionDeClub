345debc8975e85f812898d52818e2fca43e5aa41
Ramiro Trevisan <113390325+RamiTrevisan@users.noreply.github.com>
2025-04-21 15:59:12 -0300

    Revert "Realice la configuracion para implementar JWT"

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -3,12 +3,7 @@ using Application.Services;
 using Domain.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Data;
-using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Reflection;
-using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,55 +18,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(setupAction =>
-{
-    setupAction.AddSecurityDefinition("GestionClubApiBearerAuth", new OpenApiSecurityScheme() //Esto va a permitir usar swagger con el token.
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        Description = "Acá pegar el token generado al loguearse."
-    });
-
-    setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "GestionClubApiBearerAuth" } //Tiene que coincidir con el id seteado arriba en la definición
-                }, new List<string>() }
-    });
-
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    setupAction.IncludeXmlComments(xmlPath);
-
-});
-
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new()
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["AuthenticationService:Issuer"],
-            ValidAudience = builder.Configuration["AuthenticationService:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["AuthenticationService:SecretForKey"]))
-        };
-    });
-
+builder.Services.AddSwaggerGen();
 
 #region Repositories
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 #endregion
 
 #region services
-builder.Services.Configure<AuthenticationService.AuthenticationServiceOptions>(
-    builder.Configuration.GetSection(AuthenticationService.AuthenticationServiceOptions.AuthenticationService));
 builder.Services.AddScoped<IUserService, UserService>();
 
 #endregion
@@ -87,8 +40,6 @@ if (app.Environment.IsDevelopment())
 
 
 app.UseHttpsRedirection();
-
-app.UseAuthentication();
 
 app.UseAuthorization();
 
