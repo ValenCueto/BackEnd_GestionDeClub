72e9962c2074f06c4f2f0adc156fe261abf4dc82
Matias D'Anunzio <matidanunzio@gmail.com>
2025-04-21 10:03:34 -0300

    JWT completo implementado

--- src/Application/Interfaces/ICustomAuthenticationService.cs
+++ src/Application/Interfaces/ICustomAuthenticationService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.Models.Request;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
-    internal interface ICustomAuthenticationService
+    public interface ICustomAuthenticationService
     {
+        string Authenticate(AuthenticationRequest authenticationRequest);
     }
 }
--- src/Application/Models/Request/AuthenticationRequest.cs
+++ src/Application/Models/Request/AuthenticationRequest.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.Models.Request
 {
-    internal class AuthenticationRequest
+    public class AuthenticationRequest
     {
+        [Required]
+        public string? UserName { get; set; }
+        [Required]
+        public string? Password { get; set; }
     }
 }
