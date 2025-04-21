2b165dfe8988b92ee47316a1926368167f945267
Matias D'Anunzio <matidanunzio@gmail.com>
2025-04-21 09:34:04 -0300

    Realice la configuracion para implementar JWT

---
+++ src/Application/Models/Request/AuthenticationRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models.Request
+{
+    internal class AuthenticationRequest
+    {
+    }
+}
