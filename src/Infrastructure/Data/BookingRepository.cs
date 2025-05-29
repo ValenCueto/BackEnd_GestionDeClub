2a69cc704ad8bd587f37571b35a782221e28941a
Agustin <agustinferraresi5@gmail.com>
2025-05-29 14:06:59 -0300

    Crea metodo GetAllBookings
    este metodo trae los bookings con su user,si tuviese, y su cancha

--- src/Infrastructure/Data/BookingRepository.cs
+++ src/Infrastructure/Data/BookingRepository.cs
@@ -5,13 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using Domain.Entities;
 using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
     public class BookingRepository : BaseRepository<Booking>, IBookingRepository
     {
         public BookingRepository(ApplicationDbContext dbContext) : base(dbContext)
+        { }
+            
+        public List<Booking> GetAllBookings()
         {
+            return _dbContext.Bookings
+                .Include(b => b.Court)
+                .Include(b => b.User)
+                .ToList();
         }
     }
 }
